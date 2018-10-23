8e1c80b5645acca89aebd1b01a7d64205719c9a2
Hiroki Nakano <nkn12151018@gmail.com>
2018-10-23 22:25:26 +0000

    Finish signup user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
-  
-  get 'toppages/index'
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
