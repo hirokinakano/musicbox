81ed0f8f0e78000abeca10641854abe9896148f4
Hiroki Nakano <nkn12151018@gmail.com>
2018-11-08 16:14:23 +0000

    Remake test files

--- test/controllers/artists_controller_test.rb
+++ test/controllers/artists_controller_test.rb
@@ -1,9 +1,29 @@
 require 'test_helper'
 
 class ArtistsControllerTest < ActionDispatch::IntegrationTest
+ 
+ def setup
+   @artist = artists(:taro)
+   @other_artist = artists(:ziro)
+ end
+ 
   test "should get new" do
     get artists_new_url
     assert_response :success
   end
 
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Artist.count' do
+      delete artist_path(@artist)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_artist)
+    assert_no_difference 'Artist.count' do
+      delete artist_path(@artist)
+    end
+    assert_redirected_to root_url
+  end
 end
