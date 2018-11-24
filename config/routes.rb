ad493a5ffbbe0045276fe8cac2d1c01e10038797
Hiroki Nakano <nkn12151018@gmail.com>
2018-11-24 08:03:24 +0000

    Fixed navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
+  root to: 'toppages#index'
   devise_for :listeners, :controllers => {
     :registrations => 'listeners/registrations',
     :sessions => 'listeners/sessions'}
   get 'artists/new'
   get 'sessions/new'
-  root to: 'toppages#index'
-  get    '/signup',  to: 'users#new'
-  post   '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   post   '/artists/new', to: 'artists#create'
-  resources :users
   resources :artists
   resources :posts, only: [:create, :destroy]
 end
