81c27af8c0299a9ce4e9b4334b677c2d68fd132f
hiroki.nakano <nkn12151018@gmail.com>
2018-12-31 13:35:22 +0900

    Fix bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require bootstrap
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
-//= require bootstrap-sprockets
